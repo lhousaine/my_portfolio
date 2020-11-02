02795e7b2f43219627cb59ba665793f11309de79
lhousaine <l.ouarhou.ensa@gmail.com>
2020-11-03 00:43:36 +0100

    Updates

---
+++ precache-manifest.a32be16a5e5ccc7fdbc6bbdfae4de5ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a61a509917f61bcfbc088f57ca7c3c",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "9ea65d1fffb299e9713c",
+    "url": "/my_portfolio/static/css/main.69e40823.chunk.css"
+  },
+  {
+    "revision": "0a26745401ba4a8ba662",
+    "url": "/my_portfolio/static/js/2.edd2fe67.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my_portfolio/static/js/2.edd2fe67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea65d1fffb299e9713c",
+    "url": "/my_portfolio/static/js/main.07b34cd1.chunk.js"
+  },
+  {
+    "revision": "a8bf27510757bf32fcd9",
+    "url": "/my_portfolio/static/js/runtime-main.a7393764.js"
+  }
+]);
